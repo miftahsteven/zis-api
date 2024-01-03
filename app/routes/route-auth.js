1cb782b65599f0509c406eff45ed7776ec6ce984
kedarnacha <kedaranugerahnachaputra@gmail.com>
2024-01-03 10:38:27 +0700

    update login limitter

--- app/routes/route-auth.js
+++ app/routes/route-auth.js
@@ -3,9 +3,19 @@ const { user } = require("../controllers");
 const multer = require("multer");
 //const upload = multer({ dest: './uploads/reports/' })
 const { authentication, authorization } = require("../../config/auth");
+const rateLimit = require('express-rate-limit');
+const loginlimiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 3,
+    onLimitReached: (req, res, options) => {
+        return res.status(429).json({
+            message: "Terlalu banyak kesalahan. Silakan ulangi dalam 10 menit.",
+        });
+    },
+});
 
 // GET localhost:8080/home => Ambil data semua dari awal
-router.post("/login", user.loginUser);
+router.post("/login", loginlimiter, user.loginUser);
 router.post("/register", user.registerUser);
 router.put("/update", authentication, user.updateUser);
 router.post("/verifed", user.verifiedUser);
